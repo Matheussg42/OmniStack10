91f8ffcf558eac4378e79b847475ea66d62df6e3
Matheus S. Gomes <matheusbhsantos2@gmail.com>
2020-01-17 14:45:09 -0300

    Resolvendo CORS

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,16 +1,21 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const routes = require('./routes');
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const routes = require("./routes");
 
 const app = express();
 
-mongoose.connect('mongodb+srv://matheus:omnistack@cluster0-9y8nv.mongodb.net/week10?retryWrites=true&w=majority', {
+mongoose.connect(
+  "mongodb+srv://matheus:omnistack@cluster0-9y8nv.mongodb.net/week10?retryWrites=true&w=majority",
+  {
     useNewUrlParser: true,
     useUnifiedTopology: true
-});
+  }
+);
 
+app.use(cors());
 app.use(express.json());
-app.use(routes)
+app.use(routes);
 
 // Métodos HTTP: GET, POST, PUT, DELETE
 
