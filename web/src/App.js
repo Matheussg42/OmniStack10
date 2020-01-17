5da266a9108361e5b149b3f043d69b2f6a33412d
Matheus S. Gomes <matheusbhsantos2@gmail.com>
2020-01-16 22:40:45 -0300

    Adicionando Front

---
+++ web/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from './Header'
+
+// Componente
+// Estado
+// Propriedade
+
+function App() {
+  return (
+    <Header />
+  );
+}
+
+export default App;
