c66e2d13c256c1e8cbb952a42486e45ffa6896f6
Matheus S. Gomes <matheusbhsantos2@gmail.com>
2020-02-26 23:36:18 -0300

    Mudando o buscar para retornar Dev e Companies

--- Backend/src/controllers/SearchController.js
+++ Backend/src/controllers/SearchController.js
@@ -1,4 +1,5 @@
 const Dev = require('../models/Dev');
+const Company = require('../models/Company');
 const parseStringAsArray = require('../utils/parseStringAsArray');
 
 module.exports = {
@@ -20,7 +21,20 @@ module.exports = {
                 }
             }
         });
+        
+        const companies = await Company.find({
+            'jobs.Techs': {$in: techsArray},
+            location:{
+                $near:{
+                    $geometry:{
+                        type: 'Point',
+                        coordinates: [longitude, latitude]
+                    },
+                    $maxDistance:10000
+                }
+            }
+        });
 
-        return response.json({ devs })
+        return response.json({ devs, companies })
     }
 }
